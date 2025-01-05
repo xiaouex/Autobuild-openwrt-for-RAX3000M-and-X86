cf532fe80f3b3ab4c94a35ea5298588fb2e91fef
xiaouex <36121363+xiaouex@users.noreply.github.com>
2025-01-05 12:22:31 +0800

    Update diy-part1.sh

--- x86/diy-part1.sh
+++ x86/diy-part1.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
 
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+sed -i '1i src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '3i src-git opentopd https://github.com/sirpdboy/sirpdboy-package' feeds.conf.default
