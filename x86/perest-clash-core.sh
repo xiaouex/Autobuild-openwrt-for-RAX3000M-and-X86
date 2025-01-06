d611db463b2856a82eb214c178b3a3be1e6dfe45
xiaouex <36121363+xiaouex@users.noreply.github.com>
2025-01-07 00:27:08 +0800

    Update perest-clash-core.sh

--- x86/perest-clash-core.sh
+++ x86/perest-clash-core.sh
@@ -13,8 +13,8 @@ mkdir -p $GITHUB_WORKSPACE/openwrt/feeds/small/luci-app-openclash/root/etc/openc
 core_path="$GITHUB_WORKSPACE/openwrt/feeds/small/luci-app-openclash/root/etc/openclash/core"
 goe_path="$GITHUB_WORKSPACE/openwrt/feeds/small/luci-app-openclash/root/etc/openclash"
 
-file=$(find . -type f -name 'mihomo-linux-amd64-alpha-*' -print -quit)
-gunzip -c $file > $core_path/clash_meta
+cd clash-core
+gunzip -c mihomo-linux-amd64-alpha-*.gz  > $core_path/clash_meta
 wget -qO- https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat > $goe_path/GeoIP.dat
 wget -qO- https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat > $goe_path/GeoSite.dat
 
