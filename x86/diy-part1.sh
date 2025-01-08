e472d28af64fe5846644fb2f2940ac8ddf18cd71
xiaouex <36121363+xiaouex@users.noreply.github.com>
2025-01-08 22:21:36 +0800

    Update diy-part1.sh

--- x86/diy-part1.sh
+++ x86/diy-part1.sh
@@ -17,4 +17,4 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
-sed -i '3i src-git opentopd https://github.com/sirpdboy/sirpdboy-package' feeds.conf.defaul
+sed -i '3i src-git opentopd https://github.com/sirpdboy/sirpdboy-package' feeds.conf.default
