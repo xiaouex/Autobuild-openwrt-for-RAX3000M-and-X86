90b973c50665cfedd51aa7416e011cd009476831
xiaouex <36121363+xiaouex@users.noreply.github.com>
2025-01-27 13:43:37 +0800

    Update diy-part1.sh

--- x86/diy-part1.sh
+++ x86/diy-part1.sh
@@ -18,3 +18,4 @@
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '3i src-git opentopd https://github.com/sirpdboy/sirpdboy-package' feeds.conf.default
+src-git autotimeset  https://github.com/sirpdboy/luci-app-autotimeset
