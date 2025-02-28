cab055d05115d051feca4bbbc25795839b1f8f76
xiaouex <36121363+xiaouex@users.noreply.github.com>
2025-03-01 00:57:36 +0800

    Update diy-part1.sh

--- x86/diy-part1.sh
+++ x86/diy-part1.sh
@@ -25,6 +25,7 @@ rm -rf package/system/ubox
 rm -rf package/kernel/mt76/patches-6.x/002-mt7603-fix-mixed-code.patch
 rm -rf package/kernel/mt76/patches-6.x/003-wifi-mt76-mt7915-fix-oops.patch
 rm -rf package/kernel/mt76/patches-6.x/101-fixes-mt7925-build-error.patch
+rm -rf package/kernel/mt76/patches-6.x/200-fix-kernel-6.12-support.patch
 mv -f $GITHUB_WORKSPACE/x86/hostapd package/network/services/hostapd
 mv -f $GITHUB_WORKSPACE/x86/Makefile_mt76 package/kernel/mt76/Makefile
 mv -f $GITHUB_WORKSPACE/x86/udebug package/libs/udebug
