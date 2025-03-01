9a5e7edbe7f887b538687d687f75440f20aa1f7c
xiaouex <36121363+xiaouex@users.noreply.github.com>
2025-03-01 13:09:40 +0800

    update setting

--- x86/diy-part1.sh
+++ x86/diy-part1.sh
@@ -19,17 +19,21 @@ sed -i '1i src-git smpackage https://github.com/kenzok8/small-package' feeds.con
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 #sed -i '3i src-git opentopd https://github.com/sirpdboy/sirpdboy-package' feeds.conf.default
 
-rm -rf package/network/services/hostapd
-rm -rf package/kernel/mt76/Makefile
-rm -rf package/system/ubox
-rm -rf package/kernel/mt76/patches-6.x/002-mt7603-fix-mixed-code.patch
-rm -rf package/kernel/mt76/patches-6.x/003-wifi-mt76-mt7915-fix-oops.patch
-rm -rf package/kernel/mt76/patches-6.x/101-fixes-mt7925-build-error.patch
-rm -rf package/kernel/mt76/patches-6.x/200-fix-kernel-6.12-support.patch
-rm -rf package/kernel/mac80211
-mv -f $GITHUB_WORKSPACE/x86/files/hostapd package/network/services/hostapd
-mv -f $GITHUB_WORKSPACE/x86/files/Makefile_mt76 package/kernel/mt76/Makefile
-mv -f $GITHUB_WORKSPACE/x86/files/udebug package/libs/udebug
-mv -f $GITHUB_WORKSPACE/x86/files/Makefile_ubus package/system/ubus/Makefile
-mv -f $GITHUB_WORKSPACE/x86/files/ubox package/system/ubox
-mv -f $GITHUB_WORKSPACE/x86/files/mac80211 package/kernel/mac80211
+#rm -rf package/network/services/hostapd
+#rm -rf package/kernel/mt76/Makefile
+#rm -rf package/system/ubox
+#rm -rf package/kernel/mt76/patches-6.x/002-mt7603-fix-mixed-code.patch
+#rm -rf package/kernel/mt76/patches-6.x/003-wifi-mt76-mt7915-fix-oops.patch
+#rm -rf package/kernel/mt76/patches-6.x/101-fixes-mt7925-build-error.patch
+#rm -rf package/kernel/mt76/patches-6.x/200-fix-kernel-6.12-support.patch
+#rm -rf package/kernel/mac80211
+#mv -f $GITHUB_WORKSPACE/x86/files/hostapd package/network/services/hostapd
+#mv -f $GITHUB_WORKSPACE/x86/files/Makefile_mt76 package/kernel/mt76/Makefile
+#mv -f $GITHUB_WORKSPACE/x86/files/udebug package/libs/udebug
+#mv -f $GITHUB_WORKSPACE/x86/files/Makefile_ubus package/system/ubus/Makefile
+#mv -f $GITHUB_WORKSPACE/x86/files/ubox package/system/ubox
+#mv -f $GITHUB_WORKSPACE/x86/files/mac80211 package/kernel/mac80211
+
+mkdir -p feeds/xiaouex
+mv -f $GITHUB_WORKSPACE/x86/files/ipv6-helper feeds/xiaouex/ipv6-helper
+mv -f $GITHUB_WORKSPACE/x86/files/luci-app-filetransfer feeds/xiaouex/luci-app-filetransfer
