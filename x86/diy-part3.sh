51426a34405e88ced4977f4fcb7b057f20fdfc19
xiaouex <36121363+xiaouex@users.noreply.github.com>
2025-03-01 21:31:20 +0800

    chanage v2ray_geodata makefile to adapt apk manager

--- x86/diy-part3.sh
+++ x86/diy-part3.sh
@@ -1,4 +1,5 @@
 mv -f $GITHUB_WORKSPACE/x86/files/luci-app-filetransfer feeds/luci/applications/luci-app-filetransfer
+mv -f $GITHUB_WORKSPACE/x86/files/Makefile_v2ray_geodata feeds/smallpackage/v2ray-geodata/Makefile
 ./scripts/feeds update -a
 ./scripts/feeds install -a 
 #rm -rf feeds/opentopd/luci-app-autotimeset
