3bef9eaa0f5592b350210339a33bcde938f0d4fa
xiaouex <36121363+xiaouex@users.noreply.github.com>
2025-05-31 17:58:24 +0800

    Update diy-part3.sh

--- x86/diy-part3.sh
+++ x86/diy-part3.sh
@@ -1,6 +1,6 @@
 mv -f $GITHUB_WORKSPACE/x86/files/luci-app-filetransfer feeds/luci/applications/luci-app-filetransfer
 mv -f $GITHUB_WORKSPACE/x86/files/Makefile_v2ray_geodata feeds/smpackage/v2ray-geodata/Makefile
-git clone https://github.com/izilzty/luci-app-temp-status feeds/luci/applications/luci-app-temp-status
+git clone https://github.com/gSpotx2f/luci-app-temp-status feeds/luci/applications/luci-app-temp-status
 #sed -i 's/PKG_VERSION:=0.4.1-r1/PKG_VERSION:=0.4.1/g' feeds/luci/applications/luci-app-temp-status/Makefile
 #sed -i '14i PKG_RELEASE:=2' feeds/luci/applications/luci-app-temp-status/Makefile
 ./scripts/feeds update -a
