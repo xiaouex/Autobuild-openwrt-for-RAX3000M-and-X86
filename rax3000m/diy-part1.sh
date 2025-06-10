1293af0fff7c9fcf8c409ffc29088febbb1b0cf8
xiaouex <36121363+xiaouex@users.noreply.github.com>
2025-06-10 18:31:37 +0800

    Add my-script for rax3000m

--- rax3000m/diy-part1.sh
+++ rax3000m/diy-part1.sh
@@ -17,16 +17,12 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 sed -i '1i src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
-#sed -i '3i src-git opentopd https://github.com/sirpdboy/sirpdboy-package' feeds.conf.default
-#sed -i '5s/.*/& bridger/' $GITHUB_WORKSPACE/openwrt/target/linux/mediatek/filogic/target.mk
-#cp -f $GITHUB_WORKSPACE/rax3000m/Config.in $GITHUB_WORKSPACE/openwrt/toolchain/Config.in
-#cp -f $GITHUB_WORKSPACE/rax3000m/Bridger_Makefile $GITHUB_WORKSPACE/openwrt/package/network/services/bridger/Makefile
 cp -f $GITHUB_WORKSPACE/rax3000m/mt7981b-cmcc-rax3000m-nand.dtso $GITHUB_WORKSPACE/openwrt/target/linux/mediatek/dts/mt7981b-cmcc-rax3000m-nand.dtso
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 
 mkdir -p package/xiaouex
 mv -f $GITHUB_WORKSPACE/rax3000m/files/ipv6-helper package/xiaouex/ipv6-helper
+mv -f $GITHUB_WORKSPACE/rax3000m/files/my-script package/xiaouex/my-script
 
 cp $GITHUB_WORKSPACE/bbr3/601-*.patch target/linux/generic/hack-6.12
 cp $GITHUB_WORKSPACE/bbr3/501-*.patch package/network/utils/iproute2/patches
