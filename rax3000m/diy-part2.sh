263014e523726b7c8190eaef4f9cb5865cfa34e1
xiaouex <36121363+xiaouex@users.noreply.github.com>
2025-07-02 20:05:31 +0800

    Add ARCH env and update diy-part2.sh for rax3000m
    Set ARCH environment variable in workflow based on DEVICE input and use it for mihomo core filename. Cleaned up rax3000m/diy-part2.sh by removing license comments and OpenClash core preset script execution.

--- rax3000m/diy-part2.sh
+++ rax3000m/diy-part2.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
+
 
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
@@ -22,8 +13,5 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 #修正连接数（by ベ七秒鱼ベ）
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
 
-# 加入OpenClash核心
-chmod -R a+x $GITHUB_WORKSPACE/rax3000m/preset-clash-core.sh
-$GITHUB_WORKSPACE/rax3000m/preset-clash-core.sh
 
 cp -f $GITHUB_WORKSPACE/rax3000m/bg1.jpg $GITHUB_WORKSPACE/openwrt/feeds/smpackage/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
