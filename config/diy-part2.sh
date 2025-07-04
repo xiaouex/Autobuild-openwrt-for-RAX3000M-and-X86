694b534146f3deb28d54556fe40cb2126754939a
xiaouex <36121363+xiaouex@users.noreply.github.com>
2025-07-04 20:38:18 +0800

    Swap diy-part2.sh and diy-part3.sh script contents
    Moved device-specific configuration and theme changes from diy-part2.sh to diy-part3.sh, and moved feed/app installation steps from diy-part3.sh to diy-part2.sh. Updated openwrt-builder.yml to execute the scripts in the new order and set the MY_DEVICE environment variable for both scripts. This refactor clarifies the build process and script responsibilities.

--- config/diy-part2.sh
+++ config/diy-part2.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-DEVICE="$MY_DEVICE"
-# 修改默认主题
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-#修正连接数（by ベ七秒鱼ベ）
-sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
-
-if [[ "$DEVICE" == "rax3000m" ]];then
-sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
-sed -i 's/ImmortalWrt/MineRouter/g' package/base-files/files/bin/config_generate
-fi
-	
+git clone --depth 1 https://github.com/gSpotx2f/luci-app-temp-status feeds/luci/applications/luci-app-temp-status
+git clone --depth 1 https://github.com/DustReliant/luci-app-filetransfer package/xiaouex/luci-app-filetransfer
+./scripts/feeds update -a
+./scripts/feeds install -a 
+sed -i 's/--set=llvm\.download-ci-llvm=true/--set=llvm.download-ci-llvm=false/' feeds/packages/lang/rust/Makefile
 
